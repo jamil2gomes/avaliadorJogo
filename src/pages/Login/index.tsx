0b9b7fecc5102602476b2c70b753c876805be56d
ja1000 <jamil.lanister@hotmail.com>
2022-04-05 18:03:56 -0300

    M - aumento do tamanho da senha

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -225,11 +225,11 @@ const Login = ({ location }: { location?: string }) => {
                                             size='lg'
                                             type="password"
                                             minLength={4}
-                                            maxLength={8}
+                                            maxLength={16}
                                             placeholder="Insira sua senha"
                                             required
                                         />
-                                        <Form.Control.Feedback type="invalid">Senha precisa ter no mínimo 4 caracteres e no máximo 8 caracteres</Form.Control.Feedback>
+                                        <Form.Control.Feedback type="invalid">Senha precisa ter no mínimo 4 caracteres e no máximo 16 caracteres</Form.Control.Feedback>
                                     </Form.Group>
                                     <Button
                                         variant="primary"
