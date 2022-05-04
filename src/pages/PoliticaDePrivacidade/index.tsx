be72598241c6d2aa56ac82d50247b82843733589
ja1000 <jamil.lanister@hotmail.com>
2022-05-04 08:17:19 -0300

    M - indentacao dos paragrafos do termo

--- src/pages/PoliticaDePrivacidade/index.tsx
+++ src/pages/PoliticaDePrivacidade/index.tsx
@@ -11,22 +11,22 @@ const PoliticaPrivacidade = () => {
         <NavBar  exibirPesquisa={false}/>
         <Container className="container">
         <h2>Política de Privacidade</h2>
-        <p>O AutGamesFinder deseja que o usuário sinta-se seguro que todas as informações fornecidas estão sendo protegidas com cuidado por nosso site.</p>
+        <p className="paragrafoIndentado">O AutGamesFinder deseja que o usuário sinta-se seguro que todas as informações fornecidas estão sendo protegidas com cuidado por nosso site.</p>
         <h3>Coleta de Informações</h3>
-        <p>Afim do usuário poder interagir  em nosso site, avaliando e escrevendo análises para os jogos, retemos informações através do registro voluntário. Ou seja, o usuário é convidado a preencher um cadastro onde informa alguns dados pessoais, como nome e email. O usuário deverá informar dados verdadeiros sobre si mesmo ao se cadastrar no AutGamesFinder.</p>
-        <p>Caso um usuário opte por não se registrar no AutGamesFinder, o acesso a avaliação da comunidade e análises de outros usuários permanecerão disponíveis normalmente nas páginas dos jogos.</p>
+        <p className="paragrafoIndentado">Afim do usuário poder interagir  em nosso site, avaliando e escrevendo análises para os jogos, retemos informações através do registro voluntário. Ou seja, o usuário é convidado a preencher um cadastro onde informa alguns dados pessoais, como nome e email. O usuário deverá informar dados verdadeiros sobre si mesmo ao se cadastrar no AutGamesFinder.</p>
+        <p className="paragrafoIndentado">Caso um usuário opte por não se registrar no AutGamesFinder, o acesso a avaliação da comunidade e análises de outros usuários permanecerão disponíveis normalmente nas páginas dos jogos.</p>
         <h3>Uso das Informações</h3>
-        <p>Seus dados são utilizados para nos ajudar a prover o melhor serviço possível no tangente a avaliação dos jogos perante julgamente da comunidade de gamers.</p>
-        <p><b>O e-mail é confidencial</b> e só o usuário poderá visualizar em qualquer circunstância, exceto quando solicitado por ordem judicial.</p>
-        <p><b>Sua senha é criptografada</b> e apenas é utilizada para o usuário fazer login no site. Recomendamos que sua senha não seja compartilhada com qualquer outra pessoa pois é a forma de garantir a identidade do usuário ao utilizar o sistema.</p>
+        <p className="paragrafoIndentado">Seus dados são utilizados para nos ajudar a prover o melhor serviço possível no tangente a avaliação dos jogos perante julgamente da comunidade de gamers.</p>
+        <p className="paragrafoIndentado"><b>O e-mail é confidencial</b> e só o usuário poderá visualizar em qualquer circunstância, exceto quando solicitado por ordem judicial.</p>
+        <p className="paragrafoIndentado"><b>Sua senha é criptografada</b> e apenas é utilizada para o usuário fazer login no site. Recomendamos que sua senha não seja compartilhada com qualquer outra pessoa pois é a forma de garantir a identidade do usuário ao utilizar o sistema.</p>
         <h3>Tratamento das Informações</h3>
-        <p>Quando um usuário se registra, a informação coletada é armazenada unicamente nos servidores do AutGamesFinder. A informação é utilizada com o único propósito de adaptar a sua experiência no site para o seu próprio benefício, além de prover ao público, avaliações fiéis dos jogos, sem comprometer sua privacidade.</p>
+        <p className="paragrafoIndentado">Quando um usuário se registra, a informação coletada é armazenada unicamente nos servidores do AutGamesFinder. A informação é utilizada com o único propósito de adaptar a sua experiência no site para o seu próprio benefício, além de prover ao público, avaliações fiéis dos jogos, sem comprometer sua privacidade.</p>
         <br/> <br/>
-            <p>
+            <p className="paragrafoIndentado">
                 <i>Atenciosamente,</i>
 
-            </p>
-            <p><i>equipe AutGamesFinder.</i></p>
+            </p >
+            <p className="paragrafoIndentado"><i>equipe AutGamesFinder.</i></p>
         </Container>
         <Footer/>
         </>
--- src/pages/TermosDeUso/index.tsx
+++ src/pages/TermosDeUso/index.tsx
@@ -11,16 +11,16 @@ const TermosDeUso = () => {
         <NavBar exibirPesquisa={false} />
         <Container className="container">
            <h2>Termos de Uso</h2>
-           <p>Ao se cadastrar na plataforma AutGamesFinder, você está concordando com esses Termos de Uso em sua integralidade.</p>
+           <p className="paragrafoIndentado">Ao se cadastrar na plataforma AutGamesFinder, você está concordando com esses Termos de Uso em sua integralidade.</p>
            <h3>Uso aceitável</h3>
-           <p>O usuário declara que é o único responsável pelas avaliações e análises que vier a realizar nas 
+           <p className="paragrafoIndentado">O usuário declara que é o único responsável pelas avaliações e análises que vier a realizar nas 
                páginas de jogos no AutGamesFinder. O AutGamesFinder não possui qualquer responsabilidade por dados falsos que porventura venham a ser publicados por qualquer usuário. O AutGamesFinder declara que não apóia, bem como não garante a veracidade e exatidão de qualquer informação, opinião ou feedback que tenha sido postado por qualquer usuário.
             </p>
-            <p>
+            <p className="paragrafoIndentado">
             O AutGamesFinder poderá, assim que tiver conhecimento, independente de aviso ou notificação prévia, excluir a conta do usuário em razão de violação de leis e regulamentos municipais, estaduais e federais, bem como violar a moral, a ordem pública e os bons costumes.
             </p>
             <h3>Conteúdo aceitável</h3>
-            <p>O usuário se compromete a obedecer as seguintes regras:</p>
+            <p className="paragrafoIndentado">O usuário se compromete a obedecer as seguintes regras:</p>
             <ul>
             <li>Realizar análises de jogos relevantes e que tenham relação com o respectivo jogo.</li>
             <li>Não utilizar qualquer tipo de informação que envolvam conteúdo difamatório, discriminatório, vexatório, ofensivo, pornográfico adulto e infantil, conteúdo que envolva insultos, provocações ou ameaças de forma geral.</li>
@@ -35,25 +35,25 @@ const TermosDeUso = () => {
             <li>Não divulgar dados pessoais, como por exemplo, numero do seu telefone, e-mail pessoal, endereço, entre outros.</li>
             </ul>
             <h3>Direitos autorais e marcas registradas</h3>
-            <p><b>Utilizamos imagens de capa de jogo únicamente para prover informação visual 
+            <p className="paragrafoIndentado"><b>Utilizamos imagens de capa de jogo únicamente para prover informação visual 
                 indispensável à compreensão do usuário</b>. Por este motivo <b>utilizamos imagens em pequenas dimensões</b>. As imagens de capa de jogo são legalmente reconhecidos pela desenvolvedora / publicadora do respectivo jogo.
             </p>
-            <p>
+            <p className="paragrafoIndentado">
             As marcas registradas, nomes, logotipos e marcas de serviço mostrados são, legalmente reconhecidos ou não, do AutGamesFinder. Nada contido nesse site deve ser entendido como uma forma de conceder licença ou direito de usar qualquer marca registrada sem a autorização prévia escrita do AutGamesFinder. O conteúdo textual apresentado nesse site é propriedade de seu respectivo autor e não pode ser reproduzido total ou parcialmente sem sua expressa permissão escrita.
             </p>
             <h3>Legislação vigente</h3>
-            <p>
+            <p className="paragrafoIndentado">
               Esse acordo deve ser regido em todos os aspectos pelas leis da República Federativa do Brasil e do Estado do Maranhão. Todas as partes irrevogavelmente se submetem à exclusiva jurisdição pessoal dos tribunais federais e estaduais localizados no Estado do Maranhão, sempre que aplicável, para qualquer assunto decorrente ou relacionado a esse acordo, exceto aqueles que envolvem ordens ou julgamentos desses mesmos tribunais, caso em que as jurisdições pessoais não devem ser exclusivas.
             </p>
             <h3>Modificação</h3>
-            <p>
+            <p className="paragrafoIndentado">
              O AutGamesFinder reserva a si o direito de reexaminar e corrigir os termos e condições desse acordo a qualquer momento. O usuário cadastrado recebe uma notificação por email quando os termos são atualizados. Qualquer correção será obrigatória e válida imediatamente após a postagem do acordo modificado em nosso site. A continuidade do uso do site implica em concordância com qualquer alteração nos termos e condições.
             </p> <br/> <br/>
             <p>
                 <i>Atenciosamente,</i>
 
             </p>
-            <p><i>equipe AutGamesFinder.</i></p>
+            <p className="paragrafoIndentado"><i>equipe AutGamesFinder.</i></p>
             
         </Container>
         <Footer/>
